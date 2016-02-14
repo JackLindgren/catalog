4635d2e350f792d853528e19207730df49d935f5
JackLindgren <jtlindgren88@gmail.com>
2016-02-13 18:25:16 -0800

    output formatting

--- database.py
+++ database.py
@@ -1,10 +1,26 @@
 import sqlite3
 
+class book:
+        def __init__(self, title, author, country, language, topic, year):
+                self.title = title
+		self.author = author
+		self.country = country
+		self.language = language	
+		self.topic = topic
+		self.year = year
+
 conn = sqlite3.connect("lib_catalog")
 
 c = conn.cursor()
 
 GermanBooks = c.execute("SELECT * FROM books WHERE language is 'German'")
 
-for book in GermanBooks:
-	print book[1]
+bookObjects = []
+
+for entry in GermanBooks:
+	bookObjects.append(book(entry[1], entry[2], entry[3], entry[4], entry[5], entry[6] ))
+
+for item in bookObjects:
+	print item.title[:15].ljust(15), item.author[:15].ljust(15)
+
+#>>> print "abc".ljust(10), "def".ljust(10)
