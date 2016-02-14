6b02d26a9c45d8e0abf3494d764d0f16112ea82a
JackLindgren <jtlindgren88@gmail.com>
2016-02-13 17:41:11 -0800

    scrolling and page limits

--- cursing.py
+++ cursing.py
@@ -1,8 +1,8 @@
 import curses
 
 actions = []
-for i in range(1, 60):
-	actions.append("{0}\n".format(i))
+for i in range(1, 73):
+	actions.append("line number: {0}\n".format(i))
 
 def paginate(p):
 	# takes a page number and returns the items belonging to that page
@@ -15,8 +15,22 @@ def paginate(p):
 	end   = p * pageLength
 	return actions[start:end]
 
+def lastPage(p):
+	pageLength = 15
+	if len(actions) % pageLength == 0:
+		pageCount = len(actions) / pageLength
+	else:
+		pageCount = ( len(actions) / pageLength ) + 1
+	if p == pageCount:
+		return True
+	else:
+		return False
+
 def Nscreen(n, items):
-	screen.addstr("Back\n")
+	if n == -1:
+		screen.addstr("Back\n", curses.A_REVERSE)
+	else:
+		screen.addstr("Back\n")
 	i = 0
 	while i < len(items):
 		if i == n:
@@ -24,7 +38,10 @@ def Nscreen(n, items):
 		else:
 			screen.addstr(items[i])
 		i = i + 1
-	screen.addstr("Next\n")
+	if n == len(items):
+		screen.addstr("Next\n", curses.A_REVERSE)
+	else:
+		screen.addstr("Next\n")
 
 def EnterScreen(k, items):
 	screen.addstr("You pressed a button!\n")
@@ -37,16 +54,33 @@ def main(row, page):
 
 	while True:
 		event = screen.getch()
-#		items = paginate(page)
+		items = paginate(page)
 		if event == ord("q"): break
 		elif event == curses.KEY_DOWN and row != len(items) - 1:
+			# draw the next screen down
 			screen.clear()
-			Nscreen(row + 1, items)
 			row = row + 1
-		elif event == curses.KEY_UP and row != 0:
+			Nscreen(row, items)
+		elif event == curses.KEY_DOWN and row == len(items) - 1 and (lastPage(page) != True):
+			# go down a page and draw the next screen
 			screen.clear()
-			Nscreen(row - 1, items)
+			page = page + 1
+			items = paginate(page)
+			row = 0
+			Nscreen(row, items)
+
+		elif event == curses.KEY_UP and row != 0:
+			# draw the next screen up
+			screen.clear()
 			row = row - 1
+			Nscreen(row, items)
+		elif event == curses.KEY_UP and row == 0 and page != 1:
+			screen.clear()
+			page = page - 1
+			items = paginate(page)
+			row = 14
+			Nscreen(row, items)
+
 		elif event == 10:
 			screen.clear()
 			EnterScreen(row, items)
