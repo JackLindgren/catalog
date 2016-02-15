9094f53f4b2a0786f27326820cdd863cde2d601b
JackLindgren <jtlindgren88@gmail.com>
2016-02-15 11:49:34 -0800

    pagination based on screen size

--- cursing.py
+++ cursing.py
@@ -1,4 +1,5 @@
 import curses
+import os
 
 actions = []
 for i in range(1, 73):
@@ -6,7 +7,11 @@ for i in range(1, 73):
 
 def paginate(p):
 	# takes a page number and returns the items belonging to that page
-	pageLength = 15	#each page will have max 15 items
+	# useRows is a PageInfo object which will have a "rows" attribute
+	# which tells us the number of rows available on the current terminal
+	useRows = PageInfo()
+	pageLength = useRows.rows - 5
+
 	if len(actions) % pageLength == 0:
 		pageCount = len(actions) / pageLength
 	else:
@@ -14,6 +19,17 @@ def paginate(p):
 	start = (p * pageLength) - pageLength
 	end   = p * pageLength
 	return actions[start:end]
+
+class PageInfo:
+	def __init__(self):
+		rows, columns = os.popen('stty size', 'r').read().split()
+		self.author = int((8.0  / 34) * int(columns))
+		self.title  = int((12.0 / 34) * int(columns))
+		self.year   = 5
+		self.country= int((4.0  / 34) * int(columns))
+		self.lang   = int((4.0  / 34) * int(columns))
+		self.subj   = int((4.0  / 34) * int(columns))
+		self.rows   = int(rows)
 
 # tput cols - then we can get the number of columns 
 # which will let us divide the fields proportionally
@@ -29,7 +45,9 @@ def paginate(p):
 # subj   = 4  / 34
 
 def lastPage(p):
-	pageLength = 15
+	useRows = PageInfo()
+	pageLength = useRows.rows - 5
+#	pageLength = 15
 	if len(actions) % pageLength == 0:
 		pageCount = len(actions) / pageLength
 	else:
@@ -91,7 +109,12 @@ def main(row, page):
 			screen.clear()
 			page = page - 1
 			items = paginate(page)
-			row = 14
+
+			useRows = PageInfo()
+			pageLength = useRows.rows - 6
+
+			#row = 14
+			row = pageLength
 			Nscreen(row, items)
 
 		elif event == 10:
